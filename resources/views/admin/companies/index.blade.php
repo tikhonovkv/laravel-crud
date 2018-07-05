39efed81f3833bd80d6dd2578b09a459fb9e0772
tikhonov <tikhonov.kv@ya.ru>
2018-07-05 14:28:21 +0300

    Реализация API CRUD компаний

---
+++ resources/views/admin/companies/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Companies</div>
+                    <div class="panel-body">
+                        Здесь пока ничего нет...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
